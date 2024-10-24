15c00df8477d925dd7674282ea5ab13e7c1f6b47
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-10-24 18:51:29 +0300

    .

--- Sınıf Soruları/Merge.c
+++ Sınıf Soruları/Merge.c
@@ -8,60 +8,52 @@ typedef struct Node {
     struct Node* next;
 } Node;
 
-// Listeye sonuna düğüm ekleyen fonksiyon
-void addLast(Node** head, int data) {
+// Listeye sonuna düğüm ekleyen fonksiyon (yeni başı döndüren)
+Node* addLast(Node* head, int newData) {
     Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
+    newNode->data = newData;
     newNode->next = NULL;
 
-    if (*head == NULL) {
-        // Liste boşsa, yeni düğüm baş olur
-        *head = newNode;
-    } else {
-        // Liste doluysa, son düğümü bul
-        Node* temp = *head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        // Son düğümün next alanına yeni düğümü ekle
-        temp->next = newNode;
+    if (head == NULL) {
+        return newNode; // Eğer liste boşsa, yeni düğümü baş olarak döndür
     }
+
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next; // Son düğüme kadar git
+    }
+
+    temp->next = newNode; // Son düğümün next'ine yeni düğümü ekle
+    return head; // Başın adresini döndür
 }
 
 // İki sıralı listeyi birleştiren fonksiyon
 Node* mergeSortedLists(Node* head1, Node* head2) {
-    Node* newHead = NULL;  // Yeni liste başlangıcı
+    Node* newHead = NULL;  // Yeni birleştirilmiş liste
 
     // İki listeyi birleştiriyoruz
     while (head1 != NULL && head2 != NULL) {
         if (head1->data <= head2->data) {
-            addLast(&newHead, head1->data);
+            newHead = addLast(newHead, head1->data); // Yeni başı döndürüyoruz
             head1 = head1->next;
         } else {
-            addLast(&newHead, head2->data);
+            newHead = addLast(newHead, head2->data);
             head2 = head2->next;
         }
     }
 
     // Kalan elemanları ekle (eğer varsa)
     while (head1 != NULL) {
-        addLast(&newHead, head1->data);
+        newHead = addLast(newHead, head1->data);
         head1 = head1->next;
     }
+
     while (head2 != NULL) {
-        addLast(&newHead, head2->data);
+        newHead = addLast(newHead, head2->data);
         head2 = head2->next;
     }
 
     return newHead;
-}
-
-// Yeni bir düğüm oluşturma fonksiyonu (sadece örnek için)
-Node* createNode(int data) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
 }
 
 // Bir bağlı listeyi yazdırma fonksiyonu
@@ -74,15 +66,26 @@ void printList(Node* head) {
 }
 
 int main() {
+    Node* head1 = NULL;
+    Node* head2 = NULL;
+
     // İlk sıralı listeyi oluşturma: 1 -> 3 -> 5
-    Node* head1 = createNode(1);
-    head1->next = createNode(3);
-    head1->next->next = createNode(5);
+    head1 = addLast(head1, 1);
+    head1 = addLast(head1, 3);
+    head1 = addLast(head1, 5);
 
     // İkinci sıralı listeyi oluşturma: 2 -> 4 -> 6
-    Node* head2 = createNode(2);
-    head2->next = createNode(4);
-    head2->next->next = createNode(6);
+    head2 = addLast(head2, 2);
+    head2 = addLast(head2, 4);
+    head2 = addLast(head2, 6);
+
+    // İlk listeyi yazdırma
+    printf("Liste 1: ");
+    printList(head1);
+
+    // İkinci listeyi yazdırma
+    printf("Liste 2: ");
+    printList(head2);
 
     // İki sıralı listeyi birleştirme
     Node* newHead = mergeSortedLists(head1, head2);
@@ -92,15 +95,6 @@ int main() {
     printList(newHead);
 
     // Belleği serbest bırakmayı unutma
-    free(head1->next->next);
-    free(head1->next);
-    free(head1);
-
-    free(head2->next->next);
-    free(head2->next);
-    free(head2);
-
-    // Yeni birleştirilmiş listenin elemanlarını serbest bırakmayı unutma
     while (newHead != NULL) {
         Node* temp = newHead;
         newHead = newHead->next;
