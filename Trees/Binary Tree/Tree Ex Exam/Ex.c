f458551ca7308f8e1297f12ba969ce84313c2967
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-11-12 11:34:43 +0300

    85 IS BIGGEST AMCIKKKK

--- Trees/Binary Tree/Tree Ex Exam/Ex.c
+++ Trees/Binary Tree/Tree Ex Exam/Ex.c
@@ -1,6 +1,8 @@
+// 85 is biggest.
 #include <stdio.h>
 #include <stdlib.h>
-#define Max 100
+
+#define Max 100  // Stack için maksimum eleman sayısı
 
 // Binary Tree (BTREE) yapısının tanımı
 typedef struct BTREE {
@@ -9,13 +11,12 @@ typedef struct BTREE {
     struct BTREE *right;
 } BTREE;
 
-// Stack yapısının tanımı (int türü için)
+// Stack yapısının tanımı
 typedef struct Stack {
-    int data[Max];  // 100 elemanlı dizi, int türünde
-    int top;
-    int counter;  // Stack'teki eleman sayısını tutan counter
+    int data[Max];  // Stack elemanları
+    int top;         // Stack'teki son eklenen elemanın indeksi
+    int counter;     // Stack'teki eleman sayısını tutan sayaç
 } Stack;
-
 
 // Stack'in dolu olup olmadığını kontrol eden fonksiyon
 int isFull(Stack *s) {
@@ -29,14 +30,15 @@ int isEmpty(Stack *s) {
 
 // Stack'e eleman ekleyen fonksiyon
 void push(Stack *s, int value) {
-    if (isFull(s)) {  // Eğer stack doluysa, isFull ile kontrol et
+    if (isFull(s)) {  // Eğer stack doluysa
         printf("Stack is full!\n");
     } else {
-        s->data[s->top++] = value;  // Elemanı stack'e ekle
-        s->counter++;               // Counter'ı artır
+        s->data[++(s->top)] = value;  // Elemanı stack'e ekle ve top'ı artır
+        s->counter++;  // Counter'ı artır
     }
 }
-// Yeni bir düğüm oluşturma
+
+// Yeni bir BTREE düğümü oluşturma
 BTREE* newNode(int data) {
     BTREE* node = (BTREE*)malloc(sizeof(BTREE));
     node->data = data;
@@ -49,8 +51,8 @@ int levelSum(BTREE *root, int level) {
     if (root == NULL) {
         return 0;
     }
-    if (level == 0) {
-        return root->data;  // Direk int değeri döndürüyoruz
+    if (level == 1) {
+        return root->data;  // Kök seviyesini 1 kabul ediyoruz
     } else {
         return levelSum(root->left, level - 1) + levelSum(root->right, level - 1);
     }
@@ -69,7 +71,7 @@ int height(BTREE *root) {
 // Her seviyedeki toplamları stack'e ekleyen fonksiyon
 Stack sumLevelToStack(BTREE *root, Stack s) {
     int h = height(root);  // Ağacın yüksekliği
-    for (int i = 0; i <= h; i++) {
+    for (int i = 1; i <= h+1; i++) {  // Seviye 1'den başlar
         int sum = levelSum(root, i);
         push(&s, sum);  // int değeri stack'e ekle
     }
@@ -77,15 +79,15 @@ Stack sumLevelToStack(BTREE *root, Stack s) {
 }
 
 // Stack'in içeriğini yazdıran fonksiyon
-void printStack(Stack s) {
-    if (isEmpty(&s)) {
+void printStack(Stack *s) {
+    if (isEmpty(s)) {
         printf("Stack is empty.\n");
         return;
     }
 
     printf("Stack elements: ");
-    for (int i = 0; i < s.top; i++) {  // stack'teki elemanları top'a kadar yazdır
-        printf("%d ", s.data[i]);
+    for (int i = 0; i <= s->top; i++) {  // top'a kadar yazdır
+        printf("%d ", s->data[i]);
     }
     printf("\n");
 }
@@ -99,16 +101,16 @@ int main() {
     root->left->right = newNode(5);
     root->right->right = newNode(6);
 
-    // Stack önceden başlatıldı
+    // Stack'i başlat
     Stack s;
-    s.top = 0;
-    s.counter = 0;  // Stack başlangıçta boş
+    s.top = -1;  // Stack başlangıçta boş olmalı
+    s.counter = 0;  // Eleman sayısı 0
 
     // Her seviyedeki toplamları stack'e ekleyin
     s = sumLevelToStack(root, s);
     
     // Stack'in içeriğini yazdırma
-    printStack(s);
+    printStack(&s);
 
     return 0;
 }
